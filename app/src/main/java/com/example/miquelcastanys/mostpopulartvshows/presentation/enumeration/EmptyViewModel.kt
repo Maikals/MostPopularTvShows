362090d343a8d95d1f9a39ad84fab92493995a22
Miquel Castanys <miquicm@gmail.com>
2018-03-02 18:54:21 +0100

    Added empty view component

---
+++ app/src/main/java/com/example/miquelcastanys/mostpopulartvshows/presentation/enumeration/EmptyViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.miquelcastanys.mostpopulartvshows.presentation.enumeration
+
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
+import com.example.miquelcastanys.mostpopulartvshows.R
+
+enum class EmptyViewModel(@DrawableRes val imageId: Int, @StringRes val title: Int, @StringRes val subtitle: Int) {
+    ERROR(R.drawable.ic_empty_view, R.string.error_empty_view_title, R.string.error_empty_view_description),
+    EMPTY(R.drawable.ic_empty_view, R.string.no_elements_emtpy_view_title, R.string.no_elements_emtpy_view_description)
+}
