7419d983f900c8e7a0343b45497dede89f57ef06
Miquel Castanys <miquicm@gmail.com>
2018-03-03 21:01:16 +0100

    Added similar Tv Shows use case

---
+++ app/src/main/java/com/example/miquelcastanys/mostpopulartvshows/presentation/useCases/GetSimilarTvShowsListUseCase.kt
@@ -0,0 +1,22 @@
+package com.example.miquelcastanys.mostpopulartvshows.presentation.useCases
+
+import com.example.miquelcastanys.mostpopulartvshows.domain.source.MostPopularTvShowsSource
+import com.example.miquelcastanys.mostpopulartvshows.domain.source.MostPopularTvShowsSourceImpl
+import com.example.miquelcastanys.mostpopulartvshows.presentation.base.UseCaseCallback
+import com.example.miquelcastanys.mostpopulartvshows.presentation.model.domain.TvShowListResponse
+
+
+class GetSimilarTvShowsListUseCase(private val repository: MostPopularTvShowsSourceImpl) {
+    fun getAync(id: Int, apiKey: String, language: String, page: Int, callback: UseCaseCallback<TvShowListResponse>) {
+        repository.getSimilarTvShowsList(id, apiKey, language, page, object : MostPopularTvShowsSource.GetSimilarTvShowsListCallback {
+            override fun onSuccess(tvShowListResponse: TvShowListResponse) {
+                callback.onSuccess(tvShowListResponse)
+            }
+
+            override fun onFailure(errorCode: Int) {
+                callback.onError(errorCode)
+            }
+
+        })
+    }
+}
