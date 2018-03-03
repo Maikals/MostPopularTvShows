63876987a247a75757c8cfd38a061f97e0b05598
Miquel Castanys <miquicm@gmail.com>
2018-03-03 16:29:37 +0100

    Added TvShowDetailUseCase

---
+++ app/src/main/java/com/example/miquelcastanys/mostpopulartvshows/presentation/useCases/TvDetailShowDetailUseCase.kt
@@ -0,0 +1,22 @@
+package com.example.miquelcastanys.mostpopulartvshows.presentation.useCases
+
+import com.example.miquelcastanys.mostpopulartvshows.domain.source.MostPopularTvShowsSource
+import com.example.miquelcastanys.mostpopulartvshows.domain.source.MostPopularTvShowsSourceImpl
+import com.example.miquelcastanys.mostpopulartvshows.presentation.base.UseCaseCallback
+import com.example.miquelcastanys.mostpopulartvshows.presentation.model.domain.TvShowDetailResponse
+
+
+class TvDetailShowDetailUseCase(val repository: MostPopularTvShowsSourceImpl) {
+    fun getAsync(id: Int, apiKey: String, language: String, callback: UseCaseCallback<TvShowDetailResponse>) {
+        repository.getTvShowDetail(id, apiKey, language, object : MostPopularTvShowsSource.GetTvShowDetailCallback {
+            override fun onSuccess(tvShowDetailResponse: TvShowDetailResponse) {
+                callback.onSuccess(tvShowDetailResponse)
+            }
+
+            override fun onFailure(errorCode: Int) {
+                callback.onError(errorCode)
+            }
+
+        })
+    }
+}
