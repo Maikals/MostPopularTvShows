f83e3f0480ed9aa9702e3ed957b95adc98e2896c
Miquel Castanys <miquicm@gmail.com>
2018-03-04 23:19:24 +0100

    Removed apiKey

--- app/src/main/java/com/example/miquelcastanys/mostpopulartvshows/presentation/util/PresentationConstants.kt
+++ app/src/main/java/com/example/miquelcastanys/mostpopulartvshows/presentation/util/PresentationConstants.kt
@@ -3,5 +3,5 @@ package com.example.miquelcastanys.mostpopulartvshows.presentation.util
 object PresentationConstants {
     const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w1280"
     const val LANGUAGE = "en-US"
-    const val API_KEY = "98d3f21f52adf59ccbf65cb76683d73b"
+    const val API_KEY = ""
 }
