d986013ebec2b97dfc7ff279316fdac184bca830
Miquel Castanys <miquicm@gmail.com>
2018-03-04 09:25:36 +0100

    Warnings fixed

--- app/src/main/java/com/example/miquelcastanys/mostpopulartvshows/presentation/tvShowDetail/TvShowDetailFragment.kt
+++ app/src/main/java/com/example/miquelcastanys/mostpopulartvshows/presentation/tvShowDetail/TvShowDetailFragment.kt
@@ -4,7 +4,6 @@ package com.example.miquelcastanys.mostpopulartvshows.presentation.tvShowDetail
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -71,7 +70,7 @@ class TvShowDetailFragment : BaseFragment(),
     override fun getTvShowDetailOk(tvShowDetail: TvShowDetail) {
         infoContainer.visibility = View.VISIBLE
         overview.text = tvShowDetail.overview
-        val ratingString = "${getString(R.string.rating)} ${tvShowDetail.rating.toString()}"
+        val ratingString = "${getString(R.string.rating)} ${tvShowDetail.rating}"
         tvShowRating.text = ratingString
     }
 
